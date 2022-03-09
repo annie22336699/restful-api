d79eaa02672f75140b6189ae7af7406076122ef9
Cloud Cheng <annie22336699@hotmail.com>
2022-03-09 14:59:50 +0800

    form表單

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,43 +1,77 @@
+import { useEffect, useState } from 'react';
+import config from './Config';
+
 function MyForm() {
+  const [row, setRow] = useState({});
+  const API = config.MYFORM_API + `/1`;
+
+  useEffect(() => {
+    (async () => {
+      const obj = await (await fetch(API)).json();
+      console.log(obj);
+      setRow(obj[0]);
+    })();
+  }, []);
+  console.log(row);
+
+  const whenChangedAvatar = (event) => {
+    console.log(event.target.files[0]);
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      document.querySelector('#myImg').src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]); // 讀取資料
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-6">
           <form>
             <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">
-                Email address
+              <label htmlFor="account" className="form-label">
+                Account
               </label>
               <input
-                type="email"
+                type="text"
                 className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                disabled
+                value={row.account || ''}
               />
-              <div id="emailHelp" className="form-text">
-                We'll never share your email with anyone else.
-              </div>
             </div>
             <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">
-                Password
+              <label htmlFor="nickname" className="form-label">
+                Nickname
               </label>
               <input
-                type="password"
+                type="text"
                 className="form-control"
-                id="exampleInputPassword1"
+                id="nickname"
+                name="nickname"
+                defaultValue={row.nickname}
               />
             </div>
-            <div className="mb-3 form-check">
+            <div className="input-group">
               <input
-                type="checkbox"
-                className="form-check-input"
-                id="exampleCheck1"
+                type="file"
+                className="form-control"
+                id="avatar"
+                name="avatar"
+                onChange={whenChangedAvatar}
+                aria-describedby="inputGroupFileAddon04"
+                aria-label="Upload"
               />
-              <label className="form-check-label" for="exampleCheck1">
-                Check me out
-              </label>
             </div>
+            <div className="input-group">
+              <img
+                src={config.IMG_PATH + '/' + (row.avatar || 'default.jpg')}
+                alt=""
+                id="myImg"
+                style={{ maxWidth: '200px' }}
+              />
+            </div>
+            <div className="mb-3"></div>
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
